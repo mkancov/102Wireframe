820e260247cad8b4f064fd397c9cb04a25796b2b
Michael Kancov <michael.kancov00@gmail.com>
2022-08-05 14:15:59 -0400

    My loop

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ if (confirm("You are about to enter the world of Naruto!")) {
   }
 
 prompt("What is your favorite anime?")
+
+
+  let power=prompt ("What would you rate my page , 1-5");
+  for (let i=0; i<power;i++) {
+    document.write("<img src = 'https://2.bp.blogspot.com/-u3yfAwpZP-Q/XGf26jKrDWI/AAAAAAAAAGM/VF3bcoRG7xcPld6n0pUIDcdzT5uXpxAHACLcBGAs/s1600/rasengan.jpg'/>")
+
+  }
